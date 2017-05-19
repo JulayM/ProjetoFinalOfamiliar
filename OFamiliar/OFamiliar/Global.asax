1cab297599b218a93a65b170216f131f871f575c
JulayM <jm513@hotmail.com>
2017-05-19 11:10:24 +0100

    Início do projeto

---
+++ OFamiliar/OFamiliar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OFamiliar.MvcApplication" Language="C#" %>
---
+++ OFamiliar/OFamiliar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OFamiliar.Startup))]
+namespace OFamiliar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
