25edab157a7dc6ceb182e4a4915b495c747cc0d7
JulayM <jm513@hotmail.com>
2017-05-19 11:24:43 +0100

    junção das BDs do projeto  + enable migration

--- OFamiliar/OFamiliar/Models/IdentityModels.cs
+++ OFamiliar/OFamiliar/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace OFamiliar.Models
 {
@@ -20,8 +21,11 @@ namespace OFamiliar.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+
+        // dados referentes 'a BD do projeto
+        //Representar um 'construtor' desta classe
+        //Representa onde se encontra a Base de Dados
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
@@ -29,5 +33,44 @@ namespace OFamiliar.Models
         {
             return new ApplicationDbContext();
         }
+
+
+
+        //Descreve as tabelas que estao na DB
+        public virtual DbSet<Pessoas> Pessoas { get; set; }
+        public virtual DbSet<Convite> Convites { get; set; }
+        public virtual DbSet<Familia> Familias { get; set; }
+        public virtual DbSet<Movimentos> Movimentos { get; set; }
+        public virtual DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // criar a FK para a tabela dos Convites,
+            // relacionando os 'destinatários' com um 'convite'
+            modelBuilder.Entity<Convite>()
+                       .HasRequired(c => c.Destinatario)
+                       .WithMany(p => p.ListaConvitesRecebidos)
+                       .HasForeignKey(c => c.DestinatarioFK)
+                       .WillCascadeOnDelete(false);
+
+            // criar a FK para a tabela dos Convites,
+            // relacionando os 'emissores' com um 'convite'
+            modelBuilder.Entity<Convite>()
+                       .HasRequired(c => c.Emissor)
+                       .WithMany(p => p.ListaConvitesEmitidos)
+                       .HasForeignKey(c => c.EmissorFK)
+                       .WillCascadeOnDelete(false);
+            // não podemos usar a chave seguinte, nesta geração de tabelas
+            // por causa das tabelas do Identity (gestão de utilizadores)
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
     }
 }
--- OFamiliar/OFamiliar/Models/OrcamentoF_DB.cs
+++ OFamiliar/OFamiliar/Models/OrcamentoF_DB.cs
@@ -9,43 +9,43 @@ namespace OFamiliar.Models
 {
     //superclasse DbContext
     //seqüência de conexão
-    public class OrcamentoF_DB : DbContext
+    public class OrcamentoF_DB 
     {
-        //Representar um 'construtor' desta classe
-        //Representa onde se encontra a Base de Dados
-        public OrcamentoF_DB() : base("DefaultConnection") { }
+        ////Representar um 'construtor' desta classe
+        ////Representa onde se encontra a Base de Dados
+        //public OrcamentoF_DB() : base("DefaultConnection") { }
 
-        //Descreve as tabelas que estao na DB
-        public virtual DbSet<Pessoas> Pessoas { get; set; }
-        public virtual DbSet<Convite> Convites { get; set; }
-        public virtual DbSet<Familia> Familias { get; set; }
-        public virtual DbSet<Movimentos> Movimentos { get; set; }
-        public virtual DbSet<Categoria> Categorias { get; set; }
+        ////Descreve as tabelas que estao na DB
+        //public virtual DbSet<Pessoas> Pessoas { get; set; }
+        //public virtual DbSet<Convite> Convites { get; set; }
+        //public virtual DbSet<Familia> Familias { get; set; }
+        //public virtual DbSet<Movimentos> Movimentos { get; set; }
+        //public virtual DbSet<Categoria> Categorias { get; set; }
 
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            // criar a FK para a tabela dos Convites,
-            // relacionando os 'destinatários' com um 'convite'
-            modelBuilder.Entity<Convite>()
-                       .HasRequired(c => c.Destinatario)
-                       .WithMany(p => p.ListaConvitesRecebidos)
-                       .HasForeignKey(c => c.DestinatarioFK)
-                       .WillCascadeOnDelete(false);
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    // criar a FK para a tabela dos Convites,
+        //    // relacionando os 'destinatários' com um 'convite'
+        //    modelBuilder.Entity<Convite>()
+        //               .HasRequired(c => c.Destinatario)
+        //               .WithMany(p => p.ListaConvitesRecebidos)
+        //               .HasForeignKey(c => c.DestinatarioFK)
+        //               .WillCascadeOnDelete(false);
 
-            // criar a FK para a tabela dos Convites,
-            // relacionando os 'emissores' com um 'convite'
-            modelBuilder.Entity<Convite>()
-                       .HasRequired(c => c.Emissor)
-                       .WithMany(p => p.ListaConvitesEmitidos)
-                       .HasForeignKey(c => c.EmissorFK)
-                       .WillCascadeOnDelete(false);
-            // não podemos usar a chave seguinte, nesta geração de tabelas
-            // por causa das tabelas do Identity (gestão de utilizadores)
-            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
-            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        //    // criar a FK para a tabela dos Convites,
+        //    // relacionando os 'emissores' com um 'convite'
+        //    modelBuilder.Entity<Convite>()
+        //               .HasRequired(c => c.Emissor)
+        //               .WithMany(p => p.ListaConvitesEmitidos)
+        //               .HasForeignKey(c => c.EmissorFK)
+        //               .WillCascadeOnDelete(false);
+        //    // não podemos usar a chave seguinte, nesta geração de tabelas
+        //    // por causa das tabelas do Identity (gestão de utilizadores)
+        //    modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        //    modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
 
-            base.OnModelCreating(modelBuilder);
-        }
+        //    base.OnModelCreating(modelBuilder);
+        //}
     }
 }
