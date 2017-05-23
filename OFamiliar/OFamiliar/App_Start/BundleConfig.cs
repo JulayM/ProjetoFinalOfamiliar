e767b827f11b1c3415e7257383f8f19746486309
JulayM <jm513@hotmail.com>
2017-05-23 10:53:24 +0100

    criar layout da aplicação

--- OFamiliar/OFamiliar/App_Start/BundleConfig.cs
+++ OFamiliar/OFamiliar/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace OFamiliar
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                     "~/Content/bootstrap-flaty.css",
+                     //"~/Content/freelancer.css",
                       "~/Content/site.css"));
         }
     }
